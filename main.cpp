ca9336b10b604ea12cbbaac3d96a580fac850853
rafidoth <asdarafi@gmail.com>
2022-04-15 04:16:34 +0600

    2

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
+//better approach O(sqrtN)
 
 bool isPrime(int n){
 	if(n==1 || n==0) return false;
-	for(int i=2;i<n;i++){
+	for(int i=2;i*i<n;i++){
 		if(n%i==0){
 			return false;
 		}
