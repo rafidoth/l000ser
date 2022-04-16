4a097f8159ff8b573a9eaf6074c2ced76d3b14ad
rafidoth <asdarafi@gmail.com>
2022-04-16 21:01:04 +0600

    ssd

--- sieveofEratosthenes.cpp
+++ sieveofEratosthenes.cpp
@@ -7,8 +7,8 @@ bool sieveOfEratosthenes(int n){
 	v[0]=v[1]= false;
 	for(int i=2;i*i<=n;i++){
 		if(v[i]){
-			for(int j=i;i*j<=n;j++){
-				v[i*j] = false;
+			for(int j=i*i;j<=n;j+=i){
+				v[j] = false;
 			}
 		}
 	}
