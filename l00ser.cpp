987f1a9f978efe16557610851013ff52936faac8
Rafiul Hasan <67283985+rafidoth@users.noreply.github.com>
2022-04-21 16:28:47 +0600

    Update l00ser.cpp

--- l00ser.cpp
+++ l00ser.cpp
@@ -1,18 +1,35 @@
 #include <bits/stdc++.h>
-using ll = long long;
-#define pb push_back
-#define vi vector<int>
-#define endl '\n'
 using namespace std;
+//////////////////////////////////////////
+#define start int main()
+using ll = long long;
+//VECTOR RELATED//////////////////////////
+typedef vector<int> vi;
+typedef vector<long long> vl;
+typedef vector<char> vc;
 /////////////////////////////////////////
-void solve(){
+#define nil for(i=0;i<n;i++)
 
+//DEBUG//////////////////////////////////
+#define printVec for(auto x:v) cout << x<<" "
+////////////////////////////////////////
+
+void l000ser(){
+	vector<int> v(5,5);
+	printVec;
 }
-int main(){
-	int t;
-	// cin >>t;
-	t=1;
-	while(t--){
-		solve();
+
+
+
+
+start{
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+ 	int t;
+ 	cin >> t;
+ 	while(t--){
+ 		l000ser();
 	}
+
+	return 0;
 }
